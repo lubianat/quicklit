aa118575d2e697cf520bf77d8311e8d3ffbb41a5
lubianat <tiago.lubiana.alves@usp.br>
2021-03-23 10:42:00 -0300

    refactor: Extract out html tag generation

--- quicklit/utils.R
+++ quicklit/utils.R
@@ -2,39 +2,74 @@ library(stringr)
 library(httr)
 library(WikidataQueryServiceR)
 
+#' prepare_html_tags
+#'
+#' Prepares the html tags based on a list of qids
+#'
+#'  @param qids A vector containing qids
+#'  @param resource The resource to prepare the tags to. One of c("tabernacle",
+#'  "author_disambiguator", "scholia", "wikidata")
+prepare_html_tags <- function(qids, resource) {
+  if (resource == "tabernacle") {
+    name = "Tabernacle"
+  } else if (resource == "author_disambiguator") {
+    name = "Author Disambiguator"
+  } else if (resource == "scholia") {
+    name = "Scholia"
+  } else if (resource == "wikidata") {
+    name = "Wikidata"
+  }
+  
+  links <- c()
+  for (qid in qids)
+  {
+    ref = prepare_url(qid, resource)
+    link <-
+      paste0('<a target="_blank" href=', ref, ">", name, "</a>")
+    links <- c(links, link)
+  }
+  
+  return(links)
+}
+
 
 #' prepare_url
-#' 
+#'
 #' Prepares the link for a resource based on an wikidata qid for an artilce
+#'
 #' @param qid The wikidata wid
-#' @param resource The resource to prepare the url to. c("tabernacle",
-#'  "author_disambiguator", "scholia")
-prepare_url <-function(qid, resource) {
-  
-  if (resource == "tabernacle"){
+#' @param resource The resource to prepare the url to. One of c("tabernacle",
+#'  "author_disambiguator", "scholia", "wikidata")
+prepare_url <- function(qid, resource) {
+  if (resource == "tabernacle") {
     ref = paste0("https://tabernacle.toolforge.org/?#/tab/manual/",
                  qid,
                  "/P921%3BP4510")
-  } else if (resource == "author_disambiguator"){
-    ref = paste0("https://author-disambiguator.toolforge.org/work_item_oauth.php?id=",
-                 qid,
-                 "&batch_id=&match=1&author_list_id=&doit=Get+author+links+for+work")
-  } else if (resource == "scholia"){
+  } else if (resource == "author_disambiguator") {
+    ref = paste0(
+      "https://author-disambiguator.toolforge.org/work_item_oauth.php?id=",
+      qid,
+      "&batch_id=&match=1&author_list_id=&doit=Get+author+links+for+work"
+    )
+  } else if (resource == "scholia") {
     ref = paste0("https://scholia.toolforge.org/work/", qid)
+  } else if (resource == "wikidata") {
+    ref = paste0("http://www.wikidata.org/entity/", qid)
   }
+  
+  return(ref)
 }
 
 
 
 
 #' @param query The type of query, one of c("covid", "covid_brazil", "bioinfo_brazil")
-prepare_dataset_for_page <- function(query="covid"){
-  
-  if (query == "covid"){
-    articles_df <- get_covid_df() 
-  } else if (query == "covid_brazil"){
+prepare_dataset_for_page <- function(query = "covid") {
+  if (query == "covid") {
+    articles_df <- get_covid_df()
+  } else if (query == "covid_brazil") {
     articles_df <- get_covid_brazil_df()
-  }  else if (query == "bioinfo_brazil"){
+  }  else if (query == "bioinfo_brazil") {
     articles_df <- get_brazil_bioinformatics_df()
   }
   qids <- c()
@@ -42,62 +77,45 @@ prepare_dataset_for_page <- function(query="covid"){
   for (u in articles_df[["item"]])
   {
     qid <- str_replace(u, "http://www.wikidata.org/entity/", "")
-    link <- paste0('<a target="_blank" href=', u, ">", qid, "</a>")
-    links <- c(links, link)
-    qids <- c(qids,qid)
+    qids <- c(qids, qid)
   }
-  articles_df[["item"]] <- links
   articles_df[["qid"]] <- qids
-  
-  links <- c()
-  for (qid in articles_df[["qid"]])
-  {
-    ref = prepare_url(qid, "tabernacle")
-    link <- paste0('<a target="_blank" href=', ref, ">", "Tabernacle", "</a>")
-    links <- c(links, link)
-  }
-  articles_df[["Tabernacle"]] <- links
-  
-  links <- c()
-  for (qid in articles_df[["qid"]])
-  {
-    ref = prepare_url(qid, "author_disambiguator")
-    link <- paste0('<a target="_blank" href=', ref, ">", "Author Disambiguator", "</a>")
-    links <- c(links, link)
-  }
-  articles_df[["Author Disambiguator"]] <- links
-  
-  links <- c()
-  for (qid in articles_df[["qid"]])
-  {
-    ref = prepare_url(qid, "scholia")
-    link <- paste0('<a target="_blank" href=', ref, ">", "Scholia", "</a>")
-    links <- c(links, link)
-  }
-  articles_df[["Scholia"]] <- links
-  
+  articles_df[["item"]] <- NULL
+  articles_df[["Wikidata"]] <- prepare_html_tags(qids, "wikidata")
+  articles_df[["Tabernacle"]] <-
+    prepare_html_tags(qids, "tabernacle")
+  articles_df[["Author Disambiguator"]] <-
+    prepare_html_tags(qids, "author_disambiguator")
+  articles_df[["Scholia"]] <- prepare_html_tags(qids, "scholia")
+
   return(articles_df)
 }
 
 
-get_brazil_bioinformatics_df <- function(limit=6){
-  magic_number = round(runif(1, min=1, max=3000))
-  query = paste0('
-SELECT DISTINCT ?item ?label WHERE { 
+get_brazil_bioinformatics_df <- function(limit = 6) {
+  magic_number = round(runif(1, min = 1, max = 3000))
+  query = paste0(
+    '
+SELECT DISTINCT ?item ?label WHERE {
   ?item wdt:P50 ?author.
   {?author wdt:P108 | wdt:P1416 ?institution.
   ?institution wdt:P17 wd:Q155.
   ?institution wdt:P101 wd:Q128570.}
-  UNION 
+  UNION
   {?author wdt:P108 | wdt:P1416 ?institution.
   ?institution wdt:P17 wd:Q155.
   ?author wdt:P101 wd:Q128570.}
   ?item rdfs:label ?label.
   ?item wdt:P2093 ?author_name_string.
   FILTER (lang(?label)="en")
-} 
-ORDER BY ?item OFFSET ', magic_number,' LIMIT ', as.character(limit), '
-')
+}
+ORDER BY ?item OFFSET ',
+magic_number,
+' LIMIT ',
+as.character(limit),
+'
+'
+  )
   articles_df <- query_wikidata(query)
   
   return(articles_df)
@@ -106,10 +124,11 @@ ORDER BY ?item OFFSET ', magic_number,' LIMIT ', as.character(limit), '
 
 
 
-get_covid_brazil_df <-function(limit=6){
-  magic_number = round(runif(1, min=1, max=300))
-  query = paste0('
-SELECT DISTINCT ?item ?label WHERE { 
+get_covid_brazil_df <- function(limit = 6) {
+  magic_number = round(runif(1, min = 1, max = 300))
+  query = paste0(
+    '
+SELECT DISTINCT ?item ?label WHERE {
   ?item wdt:P921 wd:Q84263196.
   ?item wdt:P50 ?author.
   ?author wdt:P108 | wdt:P1416 ?institution.
@@ -117,27 +136,37 @@ SELECT DISTINCT ?item ?label WHERE {
   ?item rdfs:label ?label.
   ?item wdt:P2093 ?author_name_string.
   FILTER (lang(?label)="en")
-} 
-ORDER BY ?item OFFSET ', magic_number,' LIMIT ', as.character(limit), '
-')
+}
+ORDER BY ?item OFFSET ',
+magic_number,
+' LIMIT ',
+as.character(limit),
+'
+'
+  )
   articles_df <- query_wikidata(query)
   
   return(articles_df)
 }
 
-get_covid_df <-function(limit=6){
-  magic_number = round(runif(1, min=1, max=10000))
-  query = paste0('
-SELECT DISTINCT ?item ?label WHERE { 
+get_covid_df <- function(limit = 6) {
+  magic_number = round(runif(1, min = 1, max = 10000))
+  query = paste0(
+    '
+SELECT DISTINCT ?item ?label WHERE {
   ?item wdt:P921 wd:Q84263196.
   ?item rdfs:label ?label.
   ?item wdt:P2093 ?author_name_string.
   FILTER (lang(?label)="en")
-} 
-ORDER BY ?item OFFSET ', magic_number,' LIMIT ', as.character(limit), '
-')
+}
+ORDER BY ?item OFFSET ',
+magic_number,
+' LIMIT ',
+as.character(limit),
+'
+'
+  )
   articles_df <- query_wikidata(query)
- 
+  
   return(articles_df)
 }
-
